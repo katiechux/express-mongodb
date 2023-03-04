8f5178221062e3560299b3147e566126031c9634
Katie Chu <katiechux@gmail.com>
2023-03-04 12:57:21 -0800

    added check for administrator rights

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,15 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.route('/')
+.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find()
+  .then((campsites) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(campsites);
+  })
+  .catch(err => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
@@ -40,26 +47,27 @@ router.post('/signup', (req, res, next) => {
           });
         });
       }
-    });
-  });
+    }
+  );
+});
   
-  router.post('/login', passport.authenticate('local'), (req, res) => {
-    const token = authenticate.getToken({_id: req.user._id});
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res, next) => {
   if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');
   } else {
-    const err = new Error('You are not logged in!');
-    err.status = 401;
-    return next(err);
+      const err = new Error('You are not logged in!');
+      err.status = 401;
+      return next(err);
   }
-})
+});
 
 module.exports = router;
